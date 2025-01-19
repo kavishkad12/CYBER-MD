f9b2145e80e4b50f7ff5b992104b20205d10edb9
KD STUDIO <kd1076347@gmail.com>
2025-01-19 11:19:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ function convertToBool(text, fault = "true") {
 }
 module.exports = {
   SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
-  MONGODB: process.env.MONGODB || "Enter your mongoDB public URL",
-  OWNER_NUM: process.env.OWNER_NUM || "9470xxxxxxx",
+  MONGODB: process.env.MONGODB || "mongodb://mongo:GzqHljEYNlEuYUoeJxXtMdAowewafLRZ@viaduct.proxy.rlwy.net:51914",
+  OWNER_NUM: process.env.OWNER_NUM || "94720552487",
 };
