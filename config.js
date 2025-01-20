964db13e751e6f7487094b266a388f702c915102
KD STUDIO <kd1076347@gmail.com>
2025-01-20 21:20:07 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,8 @@ function convertToBool(text, fault = "true") {
   return text === fault ? true : false;
 }
 module.exports = {
-  SESSION_ID: process.env.SESSION_ID || "Enter your session ID",
+  SESSION_ID: process.env.SESSION_ID || "PRABATH-MD~w2lWiLrL#F958VUBlKIVv-9Djf_xulePuJom52Re4rMMD2omEY6g",
   MONGODB: process.env.MONGODB || "mongodb://mongo:GzqHljEYNlEuYUoeJxXtMdAowewafLRZ@viaduct.proxy.rlwy.net:51914",
   OWNER_NUM: process.env.OWNER_NUM || "94720552487",
 };
+  
